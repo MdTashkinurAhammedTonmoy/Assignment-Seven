e78024a648e0335e2569476cfa5662510c188be7
MdTashkinurAhammedTonmo <md11tonmoy1613@gmail.com>
2025-10-01 12:50:18 +0600

    footer complete

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="flex p-20 bg-black text-white justify-between mt-11">
+        <div className="space-y-4">
+          <h3 className="text-2xl font-bold">CS — Ticket System</h3>
+          <p className="text-[16px] text-[#A1A1AA]">
+            Lorem Ipsum is simply dummy text of the <br /> printing and typesetting
+            industry. Lorem <br />Ipsum has been the industry's standard <br /> dummy text
+            ever since the 1500s, when an <br /> unknown printer took a galley of type
+            and <br />scrambled it to make a type specimen book.
+          </p>
+        </div>
+
+        <div className="space-y-4">
+            <h3 className="text-[20px] font-bold">Company</h3>
+            <div className="text-[16px] text-[#A1A1AA] space-y-4">
+              <p >About Us</p>
+              <p >Our Mission</p>
+              <p >Contact Saled</p>
+            </div>
+        </div>
+
+        <div className="space-y-4">
+            <h3 className="text-[20px] font-bold">Services</h3>
+            <div className="text-[16px] text-[#A1A1AA] space-y-4">
+              <p>Products & Services</p>
+              <p>Customer Stories</p>
+              <p>Download Apps</p>
+            </div>
+        </div>
+
+        <div className="space-y-4">
+            <h3 className="text-[20px] font-bold">Information</h3>
+            <div className="text-[16px] text-[#A1A1AA] space-y-4">
+              <p>Privacy Policy</p>
+              <p>Terms & Conditions</p>
+              <p>Join Us</p>
+            </div>
+        </div>
+
+        <div className="space-y-4">
+            <h3 className="text-[20px] font-bold">Social Links</h3>
+            <div className="text-[16px] text-[#A1A1AA] space-y-4">
+              <p><i className="fa-brands fa-square-x-twitter"></i> @CS — Ticket System</p>
+              <p><i className="fa-brands fa-linkedin"></i> @CS — Ticket System</p>
+              <p><i className="fa-brands fa-facebook"></i> @CS — Ticket System</p>
+              <p><i className="fa-solid fa-envelope"></i> support@cst.com</p>
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
